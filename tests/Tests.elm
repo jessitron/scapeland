66b89b9ba455c7e97b88a18f35315eccad6d33d6
Jessica Kerr <jessitron@stripe.com>
2016-08-22 23:15:30 -0500

    no-op test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,6 +3,8 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import String
+import ScapelandDebug.Update
+import Model
 
 
 all : Test
@@ -10,5 +12,15 @@ all =
     describe "Something"
         [ test "When I send messages to update they get stored in the messages"
             <| \() ->
-                Expect.equal (3 + 7) 10
+                let
+                    initialModel =
+                        Model.init
+
+                    messages =
+                        []
+
+                    result =
+                        List.foldr ScapelandDebug.Update.update initialModel messages
+                in
+                    Expect.equal initialModel result
         ]
