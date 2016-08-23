57b76eb93bac3f771ff5c18ff83f4e8b6c760c91
Jessica Kerr <jessitron@stripe.com>
2016-08-22 22:54:50 -0500

    Move view into main

--- src/Main.elm
+++ src/Main.elm
@@ -2,11 +2,12 @@ module Main exposing (main)
 
 import Html exposing (Html)
 import Html.App
-import View
 import Model
 import Mouse
 import Msg exposing (Msg(MousePosition))
 import ScapelandDebug.Update
+import ScapelandDebug.View
+import Scapeland.View
 
 
 main : Program Never
@@ -14,10 +15,20 @@ main =
     { init = ( Model.init, Cmd.none )
     , update = update
     , subscriptions = \m -> Mouse.moves MousePosition
-    , view =
-        View.view
+    , view = viewWithDebug
     }
         |> Html.App.program
+
+
+viewWithDebug : Model.Model -> Html Msg
+viewWithDebug model =
+    Html.div []
+        [ Html.main' []
+            [ Scapeland.View.view model
+            ]
+        , Html.aside []
+            [ ScapelandDebug.View.view model ]
+        ]
 
 
 update msg model =
