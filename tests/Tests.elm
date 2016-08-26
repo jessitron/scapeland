1b91e9d8e1a02cd2b3ab07c601b3a482f7ea6540
Jessica Kerr <jessitron@stripe.com>
2016-08-26 13:59:47 -0500

    inline some variables

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -15,17 +15,11 @@ all =
         [ fuzz2 mousePositionX mousePositionY "When I send messages to update they get stored in the messages"
             <| \x y ->
                 let
-                    aMessage =
-                        mousePositionMessage x y
-
-                    initialModel =
-                        Model.init
-
                     messages =
-                        [ aMessage ]
+                        [ mousePositionMessage x y ]
 
                     result =
-                        List.foldr ScapelandDebug.Update.update initialModel messages
+                        List.foldr ScapelandDebug.Update.update Model.init messages
                 in
                     Expect.equal messages result.debug.messages
         ]
